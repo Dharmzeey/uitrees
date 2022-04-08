8613f6f4f89c734539c4fbb5e10e800c75bb2568
Dharmzeey <olanrewajuadam2020@gmail.com>
2022-04-08 22:37:12 +0100

    This is the message after i modify the login logout fun

--- TreesUni/settings.py
+++ TreesUni/settings.py
@@ -136,6 +136,10 @@ USE_TZ = True
 
 LOGIN_URL = 'login'
 
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
--- TreesUni/urls.py
+++ TreesUni/urls.py
@@ -17,9 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+
     path('azeezat/', admin.site.urls),
     # path('authority/', include('authority.urls')),
 
