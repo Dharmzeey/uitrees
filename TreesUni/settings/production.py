eff2fb9bc4349a22a00f8c69f0eda52dbba9cb0e
Dharmzeey <olanrewajuadam2020@gmail.com>
2022-08-27 14:50:21 +0100

    sssa

--- TreesUni/settings/production.py
+++ TreesUni/settings/production.py
@@ -9,18 +9,18 @@ ALLOWED_HOSTS = ['https://uitrees.herokuapp.com/']
 CSRF_TRUSTED_ORIGINS = ["https://uitrees.herokuapp.com/"]
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'uitrees',
-        'USER': 'postgres',
-        'PASSWORD': 'Azeezat1@',
-        'HOST': '127.0.0.1',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'uitrees',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'Azeezat1@',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '',
+    # }
 }
 
 # db_from_env = dj_database_url.config(conn_max_age=600)
