7358b05a4004e54ebcf7bfa6adacb71e80d33910
Dharmzeey <olanrewajuadam2020@gmail.com>
2022-09-11 01:50:30 +0100

    change font family

--- TreesUni/settings/base.py
+++ TreesUni/settings/base.py
@@ -1,22 +1,15 @@
-
 from pathlib import Path
 import os
 import django_heroku
-
-
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-jmiw*3lkytx163wd01ix&qm+%2(27ho70dt4!*l)8!qh7om)e-'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 
 # Application definition
 
