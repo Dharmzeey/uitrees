dc325cbf064cd4cd12ed13540ce2625246c147dd
Dharmzeey <olanrewajuadam2020@gmail.com>
2022-11-13 23:42:28 +0100

    fixed message and avatar icon

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -4,7 +4,7 @@
 const toggleMenu = document.querySelector("#menu-toggle-icon");
 const openAside = document.querySelector('#aside');
 
-function toggleFunction () {
+function toggleFunction() {
   toggleMenu.classList.toggle("activated");
   openAside.classList.toggle("activated");
 }
@@ -32,7 +32,7 @@ themeToggleBtn.addEventListener('click', () => {
 
 // THIS IS USED ON EVERY FORM PAGE TO MAKE THE LATITUDE, LONGITUDE AND COORDINATE READONLY
 
-if (document.getElementById("id_coordinates")){
+if (document.getElementById("id_coordinates")) {
   document.querySelector("#id_longitude").readOnly = true;
   document.querySelector("#id_latitude").readOnly = true;
   document.querySelector("#id_coordinates").readOnly = true;
@@ -40,28 +40,28 @@ if (document.getElementById("id_coordinates")){
 
 // THIS SCRIPT IS FOR (SPECIFIC SEARCH.HTML) THAT DYNAMICALLY EFFECT THE INPUT BEHAVIOR BASED ON THE 
 // "SEARCH TYPE ACTION" 
-if (document.getElementById("dynamic")){
+if (document.getElementById("dynamic")) {
   dynamic_input.placeholder = result
-if (result == 'Search by Location Name') {
+  if (result == 'Search by Location Name') {
     flag.style.display = 'block'
-}
+  }
 
-if (result == 'Search by Coordinates') {
+  if (result == 'Search by Coordinates') {
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
     } else {
       dynamic_input.value = "Geolocation is not supported by this browser.";
     }
-  
-  function showPosition(position) {
-    dynamic_input.value = position.coords.latitude +
-    "," + position.coords.longitude;
-  }
 
-  dynamic_input.readOnly = true;
-  document.getElementById("inp_cord").style.display = 'inline';
-}
+    function showPosition(position) {
+      dynamic_input.value = position.coords.latitude +
+        "," + position.coords.longitude;
+    }
+
+    dynamic_input.readOnly = true;
+    document.getElementById("inp_cord").style.display = 'inline';
+  }
 }
 
 
@@ -74,9 +74,9 @@ function displayIframe(val) {
   var locationTitle = val.innerText
   document.getElementById("pic_cont").style.display = 'block'
   document.getElementById("picture_title").innerText = locationTitle
-  
+
   if (window.innerWidth > 800) {
-      document.getElementById("detail-container").style.display = 'grid'
+    document.getElementById("detail-container").style.display = 'grid'
   }
 }
 
@@ -85,17 +85,19 @@ function displayIframe(val) {
 // THIS SCRIPT IS FOR (UPLOAD FORM.HTML) IT IS USED TO FETCH AND POULATE THE COORDINATE INPUT
 // WHEN USER WANTS TO UPLOAD
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(showPosition);
-} else {
-  x.value = "Geolocation is not supported by this browser.";
-}
+if (document.getElementById("id_coordinates")) {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.value = "Geolocation is not supported by this browser.";
+  }
 
-function showPosition(position) {
-x.value = position.coords.latitude +
-"," + position.coords.longitude;
-y.value = position.coords.latitude;
-z.value = position.coords.longitude;
+  function showPosition(position) {
+    x.value = position.coords.latitude +
+      "," + position.coords.longitude;
+    y.value = position.coords.latitude;
+    z.value = position.coords.longitude;
+  }
 }
 
 // THIS IS FOR POP UP MESSAGE DISAPPEAR
